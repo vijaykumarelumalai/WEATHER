8956fa68d7b64c791d50ad3912cccfe878703521
vijaykumarelumalai <131757192+vijaykumarelumalai@users.noreply.github.com>
2024-04-24 23:11:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-image: url('https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg?cs=srgb&dl=pexels-jplenio-1118873.jpg&fm=jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+#weather-container {
+    text-align: center;
+    background-color: paleturquoise;
+    border-radius: 8px;
+    padding: 20px;
+    border-color: #005ab3;
+    border-width: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    border-style: solid;
+}
+h1 {
+    color: #333333;
+}
+input[type="text"] {
+    padding: 10px;
+    border: 1px solid #cccccc;
+    border-radius: 4px;
+    margin-right: 10px;
+}
+button {
+    padding: 10px 20px;
+    background-color: #ff8c00;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+button:hover {
+    background-color: #0086b3;
+}
+#weather-info {
+    margin-top: 20px;
+}
